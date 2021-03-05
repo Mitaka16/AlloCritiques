7a3351a2cea02defe0e57d79454f0e335b4f3658
Mitaka <alanlaurier@sfr.fr>
2021-03-05 14:24:00 +0100

    Actualisation de l App avec ajout du Header et Footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        {/* Contenu de la page a mettre ici */}
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
