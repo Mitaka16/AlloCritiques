df71301dd9a4f33ff933a7ce97e2252f75454f39
Mitaka <alanlaurier@sfr.fr>
2021-03-08 16:08:14 +0100

    Implementation posters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Header_Footer/Header'
 import Footer from './Header_Footer/Footer'
+import Wishlist from './Pages/Wishlist';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Contenu de la page a mettre ici */}
+      <Wishlist />
       <Footer />
     </div>
   );
