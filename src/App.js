5f2164e324caca098b67aa8f287cd08290c25d70
Mitaka <alanlaurier@sfr.fr>
2021-03-11 15:36:13 +0100

    Ajout composant Infos_Movies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Header_Footer/Header'
 import Footer from './Header_Footer/Footer'
+import Infos_Movies from './Pages/Infos_Movies'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      ICI JE SUIS SUR LA CRITICS
+      <Infos_Movies />
       <Footer />
     </div>
   );
