43a5dafc5382db350046757871e5bfc5b9c40d1e
Theo <theo-everaere@hotmail.fr>
2021-03-11 10:59:52 +0100

    création du composant AddCritic (incomplet)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header_Footer/Header'
 import Footer from './Header_Footer/Footer'
+import AddCritic from './components/AddCritic';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      ICI JE SUIS SUR LA CRITICS
+      <AddCritic />
+
       <Footer />
     </div>
   );
