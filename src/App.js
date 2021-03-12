781db17ca6ddf3d1e55f1ad7817a7633beeb1dc3
IMEN <medouni2902@gmail.com>
2021-03-12 07:12:20 +0100

    le carousel , mais il ya un pblm de css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import "./App.css";
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Header from "./Header_Footer/Header";
 import Footer from "./Header_Footer/Footer";
-import Wishlist from "./Pages/Wishlist";
+/*import Carousel from "./HomePage/Carousel";
+import carousel from "./HomePage/carousel.css";
+import Nbr from "./HomePage/Carousel";*/
+import SearchBar from "./HomePage/Barre/Filter";
+
+  
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Switch>
+          <Switch>
           {/* Contenu de la page a mettre ici 
           <Route path='/***' component={***} */}
-        </Switch>
-      </BrowserRouter>
-      <Footer />
+      
+            <SearchBar/>
+              </Switch>
+                </BrowserRouter>
+                  <Footer />
+                  
+
     </div>
   );
 }
-
 export default App;
