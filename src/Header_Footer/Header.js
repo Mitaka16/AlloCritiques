76b26b611cd921b7fd916f8e6c20c71fa166757b
Mitaka <alanlaurier@sfr.fr>
2021-03-12 10:13:14 +0100

    Update header

--- src/Header_Footer/Header.js
+++ src/Header_Footer/Header.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
 import Logo from '../Images/Logos/Logo_AlloCritiques.png'
+import {Link} from 'react-router-dom'
 import "../Header_Footer/header.css"
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             Logged: false
+        }
+    }
+    
     render() {
-        return (
+        const {Logged} = this.state
+         return (
             <header>
                 <div className='Logo_Header'>
                     <img src={Logo} alt='Logo AlloCritiques' />
                     <h1>Allo<span style={{ color: '#EB5757' }}>Critiques</span></h1>
                 </div>
                 <div className='Bouton_Header'>
-                    <button>Accueil</button>
-                    <button>Wishlist</button>
-                    <button>Se connecter</button>
-                    <button>Inscription</button>
+                    <Link className='nav-bouton' to='/'>Accueil</Link>
+                    <Link className='nav-bouton' to='/Wishlist'>Wishlist</Link>
+                    {Logged ? <Link className='nav-bouton-account' to='/MyAccount'>Mon Compte</Link> : <Link className='nav-bouton' to='/Sign_In'>Se connecter</Link>}
+                    {Logged ? null : <Link className='nav-bouton' to='/Register'>Inscription</Link> }               
                 </div>
             </header>
         )
