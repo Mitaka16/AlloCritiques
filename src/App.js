6d3ee9528bbeeb52fda8ea6b6c9608222d32ea32
Mitaka <alanlaurier@sfr.fr>
2021-03-15 11:27:24 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header_Footer/Header";
 import Footer from "./Header_Footer/Footer";
-<<<<<<< HEAD
 import Nbr from "./HomePage/Carousel";
 import SearchBar from "./HomePage/Barre/Filter";
-=======
-// import Wishlist from "./Pages/Wishlist";
->>>>>>> origin
 
 function App() {
   return (
